b97e4a95d3ea7adcb530e2c37818c237e58e3d89
Kishan Poddar <kishanpoddar2002@gmail.com>
2022-07-22 00:05:43 +0530

    Made Our Contact Us Page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,73 +7,33 @@ const Contact = () => {
   return (
     <>
       <section id="contact">
+        <h1 class="h-primary center">Contact Us</h1>
+        <div id="contact-box">
+            <form action="">
+                <div class="form-group">
+                    <label for="name:">Name:</label>
+                    <input type="text" name="name" id="name" placeholder="Enter your name"/>
+                </div>
 
-        <h1 className="section-header">Contact</h1>
+                <div class="form-group">
+                    <label for="Email:">Email:</label>
+                    <input type="email" name="email" id="email" placeholder="Enter your Email"/>
+                </div>
 
-        <div className="contact-wrapper">
+                <div class="form-group">
+                    <label for="Phone:">Phone Number:</label>
+                    <input type="phone" name="phone" id="phone" placeholder="Enter your Phone Number"/>
+                </div>
 
+                <div class="form-group">
+                    <label for="name:">Message:</label>
+                    <textarea name="message" id="message" cols="30" rows="10"
+                        placeholder="Enter the message you want to say"></textarea>
+                </div>
 
-          <form id="contact-form" className="form-horizontal" role="form">
-
-            <div className="form-group">
-              <div className="col-sm-12">
-                <input type="text" className="form-control" id="name" placeholder="NAME" name="name" value="" required />
-              </div>
-            </div>
-
-            <div className="form-group">
-              <div className="col-sm-12">
-                <input type="email" className="form-control" id="email" placeholder="EMAIL" name="email" value="" required />
-              </div>
-            </div>
-
-            <textarea className="form-control" rows="10" placeholder="MESSAGE" name="message" required></textarea>
-
-            <button className="btn btn-primary send-button" id="submit" type="submit" value="SEND">
-              <div className="alt-send-button">
-                <i className="fa fa-paper-plane"></i><span className="send-text">SEND</span>
-              </div>
-
-            </button>
-
-          </form>
-
-
-          <div className="direct-contact-container">
-
-            <ul className="contact-list">
-              <li className="list-item"><i className="fa fa-map-marker fa-2x"><span className="contact-text place">City, State</span></i></li>
-
-              <li className="list-item"><i className="fa fa-phone fa-2x"><span className="contact-text phone"><a href="tel:1-212-555-5555" title="Give me a call">(212) 555-2368</a></span></i></li>
-
-              <li className="list-item"><i className="fa fa-envelope fa-2x"><span className="contact-text gmail"><a href="mailto:#" title="Send me an email">hitmeup@gmail.com</a></span></i></li>
-
-            </ul>
-
-            <hr />
-            <ul className="social-media-list">
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-github" aria-hidden="true"></i></a>
-              </li>
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-codepen" aria-hidden="true"></i></a>
-              </li>
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-twitter" aria-hidden="true"></i></a>
-              </li>
-              <li><a href="#" target="_blank" className="contact-icon">
-                <i className="fa fa-instagram" aria-hidden="true"></i></a>
-              </li>
-            </ul>
-            <hr />
-
-            <div className="copyright">&copy; ALL OF THE RIGHTS RESERVED</div>
-
-          </div>
-
+            </form>
         </div>
-
-      </section>
+    </section>
 
 
     </>
